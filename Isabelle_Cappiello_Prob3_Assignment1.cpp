9b9edc4d58d2896cb7a549949655ecf3b1dfcbb3
Isabelle Cappiello <icappiello8880@floridapoly.edu>
2025-09-05 16:53:56 +0000

    Adding Problem 3 Code

--- Isabelle_Cappiello_Prob3_Assignment1.cpp
+++ Isabelle_Cappiello_Prob3_Assignment1.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+int main() {
+    int x;
+    int remain;
+    int quotient;
+    
+    cout << "Enter an integer number to see if the given number is divisible by 4 or not: ";
+    cin >> x;
+    if(x < 0){
+      cout << "This program does not accept negative numbers." << endl;
+    }
+    else if (x > 100){
+        cout << "The entered number is greater than 100 and the square root of the number is " << sqrt(x) << endl;
+    }
+    else{
+        if(x%4==0){
+            cout << "The given number " << x << " is divisible by 4" << endl;
+            remain = 0;
+        }
+        else{
+            cout << "The given number " << x << " is divisible by 4" << endl;
+            remain = x % 4;
+        }
+        quotient = int(x/4);
+        cout << "Quotient: " << quotient << " and Remainder: " << remain;
+    }
+
+    return 0;
+}
